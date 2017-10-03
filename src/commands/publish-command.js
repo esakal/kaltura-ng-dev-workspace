97f5b87106891296cdb94b5e59e5aa74cf5495f3
Stas <staskgt@gmail.com>
2017-10-03 18:29:06 +0300

    feat: implement commit and push commands

--- src/commands/publish-command.js
+++ src/commands/publish-command.js
@@ -8,6 +8,7 @@ import fs from 'fs';
 import conventionalChangelog from 'conventional-changelog';
 import { accessSync } from 'fs-access';
 import showdown from 'showdown';
+import util from 'util';
 
 export async function handler(argv) {
   await new PublishCommand(argv._, argv).run();
@@ -18,17 +19,17 @@ export const command = 'publish';
 export const description = 'publish a new version';
 
 export const builder = {
-  "prepare": {
-    group: "Command Options:",
-    describe: "Prepare phase",
-    type: "boolean",
+  'prepare': {
+    group: 'Command Options:',
+    describe: 'Prepare phase',
+    type: 'boolean',
     default: true,
   },
-  "publish": {
-    group: "Command Options:",
-    describe: "Publish phase",
-    type: "boolean",
-    default: true
+  'publish': {
+    group: 'Command Options:',
+    describe: 'Publish phase',
+    type: 'boolean',
+    default: false // TODO [kmcng] default will be true
   }
 };
 
@@ -37,142 +38,169 @@ const pkg = require(pkgPath);
 const changelogFile = path.resolve(process.cwd(), './CHANGELOG.md');
 const changelogComponentFile = path.resolve(process.cwd(), './src/app/components/changelog/changelog-content/changelog-content.component.html');
 let configsToUpdate = {};
+let version = pkg.version;
 
 export default class PublishCommand extends Command {
   async runCommand() {
     if (this.options.prepare) {
-      // 1. determine next version according to commit history +
-      // 2. modify changelog.md file according to changes in github +
-      //    2.1. if possible: run check to make sure messages follow the conventional commit syntax
-      // 3. modify angular component that holds the change log content. (md to html conversion) +
-      // 4. update the app-config file with the application version
-      // 5. commit changes (without pushing)
       this.logger.verbose('Prepare phase');
 
-      const newVersion = await getNewVersion();
-      const changelog = await outputChangelog(newVersion);
-      updateChangelogComponent(changelog);
-      updateAppConfigVersion(newVersion);
+      this.logger.verbose('Get new version and update configs.\n');
+      version = await this.getNewVersion();
+
+      this.logger.verbose('Update changelog.\n');
+      const changelog = await this.updateChangelog(version);
+
+      this.logger.verbose('Update changelog component.\n');
+      this.updateChangelogComponent(changelog);
+
+      this.logger.verbose('Update app-config with new version.\n');
+      this.updateAppConfigVersion(version);
+
+      this.logger.verbose('Commit changes.\n');
+      await this.execCommit(version);
     }
 
     if (this.options.publish) {
-      this.logger.verbose('Publish phase');
+      this.logger.verbose('Publish phase.\n');
 
-      // 0. [optional]: make sure no changes pending commit
-      // 1. tag the version. include the relevant part from the changelog
-      // 2. invoke the publish command as suggested by standard version.
+      const hasUncommittedChanges = !!(await this.workspace.runShellCommnad('git status -s', true));
+      this.logger.silly('hasUncommittedChanges', hasUncommittedChanges);
+
+      if (hasUncommittedChanges) {
+        this.logger.warn('It seems that you have uncommitted changes.\n');
+        this.logger.warn('To perform this command you should either commit your changes or reset them.\n');
+        this.logger.warn('Aborting command.\n');
+        return;
+      }
+
+      this.logger.verbose(`Tagging release. Current version: ${ version }.\n`);
+      await this.workspace.runShellCommnad(`git tag -a ${ version } -m "${ version }"`); // TODO [kmcng] tag message
+
+      this.logger.verbose('Publishing release.\n');
+      await this.workspace.runShellCommnad('git push --follow-tags origin master')
     }
 
   }
-}
 
-// <editor-fold desc="Bump version">
-async function getNewVersion() {
-  const release = await bumpVersion();
-  const newVersion = semver.valid(release.releaseType) || semver.inc(pkg.version, release.releaseType, false);
-  updateConfigs(newVersion);
+  async getNewVersion() {
+    const release = await this.bumpVersion();
+    const newVersion = semver.valid(release.releaseType) || semver.inc(pkg.version, release.releaseType, false);
+    this.updateConfigs(newVersion);
 
-  return newVersion;
-}
+    return newVersion;
+  }
 
-function bumpVersion() {
-  return new Promise((resolve, reject) => {
-    conventionalRecommendedBump(
-      { preset: 'angular' },
-      (err, release) => err ? reject(err) : resolve(release)
-    );
-  });
-}
+  bumpVersion() {
+    return new Promise((resolve, reject) => {
+      conventionalRecommendedBump(
+        { preset: 'angular' },
+        (err, release) => err ? reject(err) : resolve(release)
+      );
+    });
+  }
 
-function updateConfigs(newVersion) {
-  configsToUpdate[path.resolve(process.cwd(), './package.json')] = false;
-  configsToUpdate[path.resolve(process.cwd(), './package-lock.json')] = false;
+  updateConfigs(newVersion) {
+    configsToUpdate[path.resolve(process.cwd(), './package.json')] = false;
+    configsToUpdate[path.resolve(process.cwd(), './package-lock.json')] = false;
 
-  Object.keys(configsToUpdate).forEach(configPath => {
+    Object.keys(configsToUpdate).forEach(configPath => {
+      try {
+        const stat = fs.lstatSync(configPath);
+        if (stat.isFile()) {
+          const config = require(configPath);
+          config.version = newVersion;
+          fs.writeFileSync(configPath, JSON.stringify(config, null, 2) + '\n', 'utf8');
+          // flag any config files that we modify the version # for
+          // as having been updated.
+          configsToUpdate[configPath] = true;
+        }
+      } catch (err) {
+        if (err.code !== 'ENOENT') {
+          this.logger.error(err.message);
+        }
+      }
+    })
+  }
+
+  updateChangelog(newVersion) {
+    return new Promise((resolve, reject) => {
+      this.createIfMissing();
+      let oldContent = fs.readFileSync(changelogFile, 'utf-8');
+      // find the position of the last release and remove header:
+      if (oldContent.indexOf('<a name=') !== -1) {
+        oldContent = oldContent.substring(oldContent.indexOf('<a name='));
+      }
+
+      let content = '';
+
+      let changelogStream = conventionalChangelog(
+        { preset: 'angular' },
+        { version: newVersion },
+        { merges: null }
+      ).on('error', err => reject(err));
+
+      changelogStream.on('data', buffer => {
+        content += buffer.toString();
+      });
+
+      changelogStream.on('end', function () {
+        const changelog = (content + oldContent).replace(/\n+$/, '\n');
+        fs.writeFileSync(changelogFile, changelog);
+        return resolve(changelog);
+      })
+    })
+  }
+
+  createIfMissing() {
     try {
-      const stat = fs.lstatSync(configPath);
-      if (stat.isFile()) {
-        const config = require(configPath);
-        config.version = newVersion;
-        fs.writeFileSync(configPath, JSON.stringify(config, null, 2) + '\n', 'utf8');
-        // flag any config files that we modify the version # for
-        // as having been updated.
-        configsToUpdate[configPath] = true;
-      }
+      accessSync(changelogFile, fs.F_OK)
     } catch (err) {
-      if (err.code !== 'ENOENT') {
-        this.logger.error(err.message);
+      if (err.code === 'ENOENT') {
+        fs.writeFileSync(changelogFile, '\n');
       }
     }
-  })
-}
+  }
 
-// </editor-fold>
+  updateChangelogComponent(changelog) {
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(changelog).replace(/[{}]+/g, '');
+    fs.writeFileSync(changelogComponentFile, html, 'utf8');
+  }
 
-// <editor-fold desc="Write changelog">
-function outputChangelog(newVersion) {
-  return new Promise((resolve, reject) => {
-    createIfMissing();
-    let oldContent = fs.readFileSync(changelogFile, 'utf-8');
-    // find the position of the last release and remove header:
-    if (oldContent.indexOf('<a name=') !== -1) {
-      oldContent = oldContent.substring(oldContent.indexOf('<a name='));
-    }
+  updateAppConfigVersion(newVersion) {
+    const filePath = path.resolve(process.cwd(), './src/app-config/index.ts');
 
-    let content = '';
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        return this.logger.error(err);
+      }
 
-    let changelogStream = conventionalChangelog(
-      { preset: 'angular' },
-      { version: newVersion },
-      { merges: null }
-    ).on('error', err => reject(err));
+      const result = data.replace(/'appVersion':.*,/g, `'appVersion': '${ newVersion }',`);
 
-    changelogStream.on('data', buffer => {
-      content += buffer.toString();
+      fs.writeFileSync(filePath, result, 'utf8');
+    });
+  }
+
+  async execCommit (newVersion) {
+    const paths = [changelogFile, changelogComponentFile];
+    const commitMessage = `Commit changes for ${ newVersion } release`; // TODO [kmcng] commit message
+    let msg = 'committing %s';
+    let toAdd = `${changelogFile} ${changelogComponentFile}`;
+
+    Object.keys(configsToUpdate).forEach(config => {
+      if (configsToUpdate[config]) {
+        msg += ' and %s';
+        paths.unshift(path.basename(config));
+        toAdd += ' ' + path.relative(process.cwd(), config);
+      }
     });
 
-    changelogStream.on('end', function () {
-      const changelog = (content + oldContent).replace(/\n+$/, '\n');
-      fs.writeFileSync(changelogFile, changelog);
-      return resolve(changelog);
-    })
-  })
-}
+    await this.workspace.runShellCommnad(`git add ${toAdd}`);
+    await this.workspace.runShellCommnad(`git commit ${toAdd} -m "${this.formatCommitMessage(commitMessage, newVersion)}"`);
+  }
 
-function createIfMissing() {
-  try {
-    accessSync(changelogFile, fs.F_OK)
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      fs.writeFileSync(changelogFile, '\n');
-    }
+  formatCommitMessage(msg, newVersion) {
+    return String(msg).indexOf('%s') !== -1 ? util.format(msg, newVersion) : msg;
   }
 }
-
-// </editor-fold>
-
-// <editor-fold desc="Update changelog component">
-function updateChangelogComponent(changelog) {
-  const converter = new showdown.Converter();
-  const html = converter.makeHtml(changelog).replace(/[{}]+/g, '');
-  fs.writeFileSync(changelogComponentFile, html, 'utf8');
-}
-
-// </editor-fold>
-
-// <editor-fold desc="Update app config version">
-function updateAppConfigVersion(newVersion) {
-  const filePath = path.resolve(process.cwd(), './src/app-config/index.ts');
-
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      return this.logger.error(err);
-    }
-
-    const result = data.replace(/"appVersion":.*,/g, `"appVersion": "${ newVersion }",`);
-
-    fs.writeFileSync(filePath, result, 'utf8');
-  });
-}
-
-// </editor-fold>
